8b2bc01c24df53ac38e51d09ddfb3be1098e348c
mohamed <mohamedmaged35@yahoo.com>
2018-07-31 15:26:33 +0200

    basic service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import {DISHES} from '../shared/dishes'
+import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,12 +8,13 @@ import {DISHES} from '../shared/dishes'
 })
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
+  dishes :Dish[];
 
   selectedDish : Dish;
-  constructor() { }
+  constructor(private dishservice: DishService ) { }
 
   ngOnInit() {
+    this.dishes= this.dishservice.getDishes();
   }
 onSelect(dish:Dish){
     this.selectedDish = dish;
